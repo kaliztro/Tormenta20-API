5d754d6455c81be271b4f0877165c35e96a4db5d
Kaliztro <kaliztro.s@gmail.com>
2024-12-30 21:47:44 -0300

    removedo codigo não usado e corrigido bug em que a pesquisa por grupo não funcionava

--- src/util.js
+++ src/util.js
@@ -12,31 +12,7 @@ module.exports = {
         return Object.keys(data).filter(key => key === normalizedOption);
     },    
     getDataByClasse(data, req) {
-        const normalizedClasse = req.params.classe
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, "")
-            .toLowerCase()
-            .replace(/\s+/g, '_');
-            console.log(normalizedClasse)
-        return Object.entries(data).filter(([key, value]) => value.classe
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, "")
-            .toLowerCase()
-            .replace(/\s+/g, '_') === normalizedClasse);
-    },
-    getDataByTipo(data, req) {
-        const normalizedTipo = req.params.tipo
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, "")
-            .toLowerCase()
-            .replace(/\s+/g, '_');
-            console.log(normalizedTipo)
-        return Object.entries(data).filter(([key, value]) => value.tipo
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, "")
-            .toLowerCase()
-            .replace(/\s+/g, '_') === normalizedTipo);
-            
-    },
+        return Object.entries(data).filter(([key, value]) => value.classe === (req.params.classe));
+    }
 }
 
