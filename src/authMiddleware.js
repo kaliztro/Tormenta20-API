ed7e739d5b8147323278f466151b9261d7a2c3ba
Kaliztro <kaliztro.s@gmail.com>
2025-02-28 21:28:23 -0300

    adicionado autenticação para rotas exceto Get

---
+++ src/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET; // Acessa a variável JWT_SECRET do .env
+
+console.log('Segredo do JWT:', secret);
+
+const authMiddleware = (req, res, next) => {
+    if (req.method !== 'GET') {
+        const token = req.headers['authorization'];
+
+        if (!token) {
+            return res.status(401).json({ message: 'Token de autenticação não fornecido' });
+        }
+
+        try {
+            const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
+            req.user = decoded;
+            next();
+        } catch (error) {
+            return res.status(401).json({ message: 'Token inválido ou expirado' });
+        }
+    } else {
+        next();
+    }
+};
+
+module.exports = authMiddleware;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
+require('dotenv').config(); 
+
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const authMiddleware = require('./authMiddleware');
+
 const app = express();
 const porta = process.env.PORT || 8080;
 
-// Middleware para habilitar CORS
 app.use(cors());
 
 // Middleware para analisar o corpo das requisições como JSON
 app.use(express.json());
+
+// Aplicar o middleware de autenticação antes das rotas
+app.use(authMiddleware);
 
 // Usar as rotas importadas
 app.use(routes);
@@ -21,7 +27,6 @@ app.use((req, res, next) => {
     });
 });
 
-// Iniciar o servidor
 app.listen(porta, () => {
     console.log(`API está sendo executada na porta ${porta}!`);
 });
